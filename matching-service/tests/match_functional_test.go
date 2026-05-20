59edc499a5167ddfae7d90bb52a65955b54fa99a
RIFKY FADHILLAH AKBAR <rifkyfadhillahakbar@gmail.com>
2026-05-20 20:47:46 +0700

    functional test +

---
+++ matching-service/tests/match_functional_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"testing"
+)
+
+func TestMatchDriver_Functional_RealDB(t *testing.T) {
+	t.Log("Menjalankan Functional Test mengecek algoritma matching dan interaksi ke Database Asli...")
+
+	// Langsung buat FAIL sesuai instruksi dosen karena DB belum siap
+	t.Fatal("Functional Test Failed: Koneksi Database asli dan algoritma Matching belum diimplementasi!")
+}
---
+++ order-service/tests/order_functional_test.go
@@ -0,0 +1,12 @@
+package tests
+
+import (
+	"testing"
+)
+
+func TestCreateOrder_Functional_RealDB(t *testing.T) {
+	t.Log("Menjalankan Functional Test mengecek interaksi Order ke Database Asli...")
+
+	// Langsung buat FAIL sesuai instruksi dosen karena DB belum siap
+	t.Fatal("Functional Test Failed: Koneksi Database asli dan repository Order belum diimplementasi!")
+}
